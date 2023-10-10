558715aac4c176e96750eba18d9b92531fc1aace
anbup <anbu@thelaunchclub.in>
2023-10-10 15:52:20 +0530

    Second commit

--- Core/Mobile/IOS/AppTheme.swift
+++ Core/Mobile/IOS/AppTheme.swift
@@ -3,12 +3,16 @@ import Foundation
 import SwiftUI
 
 class AppTheme {
+    
+    public static let lightThemeColor: Color = .white
+    public static let darkThemeColor: Color = .black
+    
+    public static let appPrimaryColor: String = "AppPrimaryColor"
     public static let buttonBaseColor: String = "ButtonBaseColor"
     public static let buttonBackgroundColor: String = "ButtonBackgroundColor"
     
-    public static let appPrimaryColor: String = "AppPrimaryColor"
-    public static let buttonTextColor: Color = .white
-    public static let lightSecondaryColor: String = "SecondaryTextColor"
+    public static let buttonTextPrimaryColor: Color = .white
+    public static let buttonTextSecondaryColor: String = "ButtonTextSecondaryColor"
 
     public static let titleColor: Color = .black
     public static let subTitleColor: String = "SubTitleColor"
@@ -16,13 +20,13 @@ class AppTheme {
     
     public static let errorColor: String = "ErrorColor"
     public static let searchBarBackgroundColor: String = "SearchBarBackgroundColor"
-    public static let outlineColor: String = "OutlineColor"
+    public static let HeaderOutlineColor: String = "HeaderOutlineColor"
 
     public static let titleSize: Int = 16
     public static let defaultFontSize: Int = 14
     public static let iconTextSize: Int = 11
     
-    public static let font: String = "Lexend"
+    public static let defaultFont: String = "Lexend"
 
     public static let titleWeight: Int = 600
     public static let defaultTextWeight: Int = 500
